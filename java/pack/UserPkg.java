49410338a2dd9cce9d1b1038de116b2db6880a86
Rajinikanth Kakarla <rajinikanthkakarla.03@gmail.com>
2023-09-12 08:37:55 +0530

    Add files via upload

---
+++ java/pack/UserPkg.java
@@ -0,0 +1,7 @@
+package pack;
+public class UserPkg {
+    public void msg(){
+        System.out.println("usrpkg: [executed] <now>");
+        System.out.println("usrpkg: [stopped] <exited>");
+    }
+}
